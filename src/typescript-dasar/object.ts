0cba0db90a43863f748b829fb7a42cabefdfcb45
andi.fivesco@gmail.com <andi.fivesco@gmail.com>
2022-04-28 17:22:25 +0700

    testing impl with filter & map

--- src/typescript-dasar/object.ts
+++ src/typescript-dasar/object.ts
@@ -1,24 +1,54 @@
-
-
-
-
-
 // variable user ini bersifat immutable
 type User = {
     name: string;
     age: number;
 }
 
-
-let user: User = {
-    name:"andi",
-    age:21
+type Nilai = {
+    score: number;
 }
 
-user =  {
-    name:"faqih",
-    age:22
+let score:Nilai
+let list: User 
+
+const listMahasiswa = [
+    list={name:"andi",age:21},
+    list={name:"ganteng",age:61},
+    list={name:"mee",age:51},
+    list={name:"tedu",age:31}
+]
+
+const listScore = [
+    score={score:30.4},
+    score={score:50.7},
+    score={score:80.90},
+    score={score:83.90},
+    score={score:84.90}
+]
+    
+//lakukan perulangan dengan map   
+let datfarUser = listMahasiswa.map(u =>{
+    return u.name
+})
+
+let datfarScore = listScore.map(s=>{
+    return s.score.toFixed() 
+})
+
+
+let filterUsia = listMahasiswa.filter(mhs =>{
+    return mhs.age > 30
+})
+
+
+function Main2():void {
+    console.log(datfarUser)
+    console.log(datfarScore)
+    console.log(filterUsia)
+
 }
 
-console.log(user)
+Main2()
+
+
 
