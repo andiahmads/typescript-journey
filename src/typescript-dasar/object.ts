4d388f7c0784be6c6ace98e22612ea970b28912e
andi.fivesco@gmail.com <andi.fivesco@gmail.com>
2022-04-28 16:41:46 +0700

    object implementation

---
+++ src/typescript-dasar/object.ts
@@ -0,0 +1,24 @@
+
+
+
+
+
+// variable user ini bersifat immutable
+type User = {
+    name: string;
+    age: number;
+}
+
+
+let user: User = {
+    name:"andi",
+    age:21
+}
+
+user =  {
+    name:"faqih",
+    age:22
+}
+
+console.log(user)
+
