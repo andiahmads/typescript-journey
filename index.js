87af2ffc85213075735e0d5e6a293c0584b8de24
andi.fivesco@gmail.com <andi.fivesco@gmail.com>
2022-04-28 12:14:40 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("hello coksss");
+var int = 1;
+while (true) {
+    int++;
+    console.log(int);
+}
---
+++ index.ts
@@ -0,0 +1,7 @@
+console.log("hello coksss");
+
+let count: number =  1
+while (true) {
+    count++
+    console.log(count)
+}
